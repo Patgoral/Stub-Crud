3020e30c7f25a7676f22f45ba59790f7ca3c4210
Jose <alex@MacBook-Pro.local>
2023-02-17 09:20:24 -0500

    adjusted pages

--- src/pages/RegistrationHistoryPage/RegistrationHistoryPage.js
+++
@@ -1,9 +0,0 @@
-import placeholder from "../../images/placeholder.jpg"
-
-export default function RegistrationHistoryPage() {
-    return (
-        <div>
-            <h1>RegistrationHistoryPage</h1>
-        </div>
-    )
-}
