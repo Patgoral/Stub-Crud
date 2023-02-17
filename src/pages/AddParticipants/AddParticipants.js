f69ab843b30c693053d3fb87fce34edbf384fe4d
speckoh <speckohart@gmail.com>
2023-02-16 20:09:44 -0500

    add participants to db

--- src/pages/AddParticipants/AddParticipants.js
+++ src/pages/AddParticipants/AddParticipants.js
@@ -1,15 +1,39 @@
-import "./AddParticipants.css"
+import {useState, useEffect } from 'react'
+import * as participantsAPI from '../../utilities/participants-api'
 
 export default function AddParticipants() {
+    const [participant, setParticipant] = useState({
+        name: '',
+        location: ''
+    })
+    //ADD A PARTICIPANT
+    async function handleAddParticipant(event){
+        event.preventDefault()
+        const participantData = { participant:participant }
+        const participantToAdd = await participantsAPI.addParticipant(participantData)
+        setParticipant(participantToAdd)
+    }
+    //handles the change in the form to ADD
+    function handleInputChange(event){
+        const addNewParticipant = {...participant, [event.target.name] : event.target.value}
+        setParticipant(addNewParticipant)
+        console.log(participant)
+    }
     return(
         <div>
             <h2 className="register-header">Register for event below</h2>
-            <form className="register-form">
-                <input placeholder="Name">
+            <form className="register-form" onSubmit={handleAddParticipant}>
+                <input placeholder='Name' 
+                name ='name'
+                value={participant.name || ''}
+                onChange={handleInputChange}>
                 </input>
-                <input placeholder="Location">
+                <input placeholder='Location'
+                name ='location'
+                value={participant.location || ''}
+                onChange={handleInputChange}>
                 </input>
-                <button type="submit">Register</button>
+                <button type='submit'>Register</button>
             </form>
         </div>
     )
