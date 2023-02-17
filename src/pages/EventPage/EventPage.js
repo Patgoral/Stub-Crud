22702dc7c5916b9d6cbe72e8226e2f5927ea67b2
Jose <alex@MacBook-Pro.local>
2023-02-17 11:37:16 -0500

    manage page and delete function

--- src/pages/EventPage/EventPage.js
+++ src/pages/EventPage/EventPage.js
@@ -32,6 +32,7 @@ export default function EventPage() {
 
 
     return (
+
         <div className='event-page'>
             <h2>Event Page</h2>
             <div className='event-page-container-top'>
@@ -42,7 +43,7 @@ export default function EventPage() {
             </div>
             <div className='event-page-container-bottom'>
                 <div className='event-desc-container'>
-                    <div className='event-desc-header'>Event Description</div>
+                    <div className='event-desc-header'>Event  Description</div>
                     <div className='event-desc-body'>This is a Very Awesome Event. Please Join Us for an Adventure!</div>
                 </div>
                 <div className='attendees-container'>
