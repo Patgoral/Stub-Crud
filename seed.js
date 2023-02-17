509b6050ca9333419f0760078e7e493e5c79bb06
Patrick Goral <patgoral@gmail.com>
2023-02-17 15:43:29 -0500

    disabled seed

--- seed.js
+++ seed.js
@@ -1,102 +1,102 @@
-require('dotenv').config()
-require('./config/database')
-const Participant = require('./models/participant')
+// require('dotenv').config()
+// require('./config/database')
+// const Participant = require('./models/participant')
 
 
-;(async function seed() {
-    await Participant.deleteMany({})
-        const participants = await Participant.create([
+// ;(async function seed() {
+//     await Participant.deleteMany({})
+//         const participants = await Participant.create([
 
-            {
-                name: "Michael Rowe",
-                location: "Huntsville, AL" 
-            },
-            {
-                name: "Lucas Gomez",
-                location: "Brevard NC" 
-            },
-            {
-                name: "Chris Slaton",
-                location: "Tallahassee, FL" 
-            },
-            {
-                name: "Danielle Slaton",
-                location: "Tallahassee, FL" 
-            },
-            {
-                name: "Grace Ingham",
-                location: "Athens, GA" 
-            },
-            {
-                name: "Jill Lowther",
-                location: "Atlanta, GA" 
-            },
-            {
-                name: "Anthony Huinker",
-                location: "Atlanta, GA" 
-            },
-            {
-                name: "Leigh Finlayson",
-                location: "Atlanta, GA"
-            },
-            {
-                name: "Kate Crockett",
-                location: "Augusta, GA" 
-            },
-            {
-                name: "Jack Crockett",
-                location: "Augusta, GA" 
-            },
-            {
-                name: "Greg Ekrem",
-                location: "Atlanta, GA" 
-            },
-            {
-                name: "Shane Byler",
-                location: "Atlanta, GA" 
-            },
-            {
-                name: "Bill Mondock",
-                location: "Warner Robbins, GA" 
-            },
-            {
-                name: "Ted Wood",
-                location: "Atlanta, GA" 
-            },
-            {
-                name: "Jeph Burgoon",
-                location: "Smyrna, GA" 
-            },
-            {
-                name: "Jacob Cronan",
-                location: "Atlanta, GA" 
-            },
-            {
-                name: "Chris Bulloch",
-                location: "Atlanta, GA" 
-            },
-            {
-                name: "Cedar Blanchard",
-                location: "Jasper, GA" 
-            },
-            {
-                name: "Olen Daelhousen",
-                location: "Brevard, NC" 
-            },
-            {
-                name: "Marcos Schechter",
-                location: "Atlanta, GA" 
-            },
-            {
-                name: "David Stearman",
-                location: "Washington D.C." 
-            },
-            {
-                name: "Kinzer Hewitt",
-                location: "Washington D.C." 
-            },
+//             {
+//                 name: "Michael Rowe",
+//                 location: "Huntsville, AL" 
+//             },
+//             {
+//                 name: "Lucas Gomez",
+//                 location: "Brevard NC" 
+//             },
+//             {
+//                 name: "Chris Slaton",
+//                 location: "Tallahassee, FL" 
+//             },
+//             {
+//                 name: "Danielle Slaton",
+//                 location: "Tallahassee, FL" 
+//             },
+//             {
+//                 name: "Grace Ingham",
+//                 location: "Athens, GA" 
+//             },
+//             {
+//                 name: "Jill Lowther",
+//                 location: "Atlanta, GA" 
+//             },
+//             {
+//                 name: "Anthony Huinker",
+//                 location: "Atlanta, GA" 
+//             },
+//             {
+//                 name: "Leigh Finlayson",
+//                 location: "Atlanta, GA"
+//             },
+//             {
+//                 name: "Kate Crockett",
+//                 location: "Augusta, GA" 
+//             },
+//             {
+//                 name: "Jack Crockett",
+//                 location: "Augusta, GA" 
+//             },
+//             {
+//                 name: "Greg Ekrem",
+//                 location: "Atlanta, GA" 
+//             },
+//             {
+//                 name: "Shane Byler",
+//                 location: "Atlanta, GA" 
+//             },
+//             {
+//                 name: "Bill Mondock",
+//                 location: "Warner Robbins, GA" 
+//             },
+//             {
+//                 name: "Ted Wood",
+//                 location: "Atlanta, GA" 
+//             },
+//             {
+//                 name: "Jeph Burgoon",
+//                 location: "Smyrna, GA" 
+//             },
+//             {
+//                 name: "Jacob Cronan",
+//                 location: "Atlanta, GA" 
+//             },
+//             {
+//                 name: "Chris Bulloch",
+//                 location: "Atlanta, GA" 
+//             },
+//             {
+//                 name: "Cedar Blanchard",
+//                 location: "Jasper, GA" 
+//             },
+//             {
+//                 name: "Olen Daelhousen",
+//                 location: "Brevard, NC" 
+//             },
+//             {
+//                 name: "Marcos Schechter",
+//                 location: "Atlanta, GA" 
+//             },
+//             {
+//                 name: "David Stearman",
+//                 location: "Washington D.C." 
+//             },
+//             {
+//                 name: "Kinzer Hewitt",
+//                 location: "Washington D.C." 
+//             },
            
 
-        ])
-        console.log(participants)
-    })()
+//         ])
+//         console.log(participants)
+//     })()
