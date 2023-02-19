4a2cd6c8a0309f3a7889ccbceaabd5012529ddb7
speckoh <speckohart@gmail.com>
2023-02-18 20:23:33 -0500

    style event page

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -16,7 +16,7 @@ export default function AuthPage({ user, setUser }) {
 			<div className='auth-container'>
 				<div className='sign-in-form'>
 					<h2>Fried Clay 200k 2023 Event</h2>
-					<hr />
+					<div className="h-divider"></div>
 					<SignUpForm setUser={setUser} user={user}
 						signUpModal={signUpModal} setSignUpModal={setSignUpModal} />
 					<LoginForm setUser={setUser}
