ebffb33e5629cdb3de61b438a75a52100cbb35ad
speckoh <speckohart@gmail.com>
2023-02-20 14:51:58 -0500

    styling edits

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -15,7 +15,7 @@ export default function AuthPage({ user, setUser }) {
 			</div>
 			<div className='login-container'>
 				<div className='sign-in-form'>
-					<h2>Fried Clay 200k 2023 Event</h2>
+					<h2 className="login-header">Fried Clay 200k 2023 Event</h2>
 					<div className="h-divider"></div>
 					<SignUpForm setUser={setUser} user={user}
 						signUpModal={signUpModal} setSignUpModal={setSignUpModal} />
