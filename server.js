056c5b638253a6edc89c4a7e1b1688adc4efeb20
Patrick Goral <patgoral@gmail.com>
2023-02-21 12:43:55 -0500

    cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const cors = require('cors')
 // Always require and configure near the top
 require('dotenv').config();
 // Connect to the database
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://127.0.0.1:5505` }))
+
 
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
