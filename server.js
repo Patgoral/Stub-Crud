011ae2ceb44f4fe4fbf4e356dd06b9b08afcc61e
Patrick Goral <patgoral@gmail.com>
2023-02-21 13:32:56 -0500

    deploytest

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:3000` }))
 
 
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(require('./config/checkToken'))
 
@@ -29,9 +29,9 @@ app.use('/api/participants', require('./routes/api/participants'))
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX/API requests
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);
