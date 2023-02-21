d21ce41ce58944bfeb9ac171c45a0eff9bd50799
Patrick Goral <patgoral@gmail.com>
2023-02-21 13:24:05 -0500

    updated calls

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://127.0.0.1:5505` }))
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:3000` }))
 
 
-// Configure both serve-favicon & static middleware
-// to serve from the production 'build' folder
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
+
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(require('./config/checkToken'))
 
@@ -30,9 +29,9 @@ app.use('/api/participants', require('./routes/api/participants'))
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX/API requests
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);
