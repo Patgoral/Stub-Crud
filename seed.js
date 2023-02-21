a44423b3429f23fc2a52f39239b29c2358e5c1e6
Patrick Goral <patgoral@gmail.com>
2023-02-21 14:54:05 -0500

    merging

--- seed.js
+++ seed.js
@@ -1,35 +1,46 @@
+<<<<<<< HEAD
 const Participant = require('./models/participant')
 
 // Set up the Eventbrite API request parameters
 const token = process.env.EVENTBRITE_TOKEN
 const eventId = "430916953207";
 const url = `https://www.eventbriteapi.com/v3/events/${eventId}/attendees/`;
+=======
 
-const headers = {
-  "Authorization": `Bearer ${token}`
-};
+// const fetch = require('node-fetch');
+// const Participant = require('./models/participant')
+
+// // Set up the Eventbrite API request parameters
+// const token = process.env.TOKEN
+// const eventId = "430916953207";
+// const url = `https://www.eventbriteapi.com/v3/events/${eventId}/attendees/`;
+>>>>>>> 10b63a7 (seed comment)
+
+// const headers = {
+//   "Authorization": `Bearer ${token}`
+// };
 
 
-async function seed() {
-        await Participant.deleteMany({})
+// async function seed() {
+//         await Participant.deleteMany({})
 
-  try {
+//   try {
   
-    // Fetch the attendee data from the Eventbrite API
-    const response = await fetch(url, { headers });
-    const data = await response.json();
+//     // Fetch the attendee data from the Eventbrite API
+//     const response = await fetch(url, { headers });
+//     const data = await response.json();
 
-    const attendees = data.attendees;
+//     const attendees = data.attendees;
 
-    // Insert the attendee data into the collection
-    const result = await Participant.insertMany(
-      attendees.map(attendee => ({ name: attendee.profile.name, location: `${attendee.profile.addresses.home.city}, ${attendee.profile.addresses.home.region}`  }))
-    );
-    console.log("Eventbrite seed documents inserted into the collection");
+//     // Insert the attendee data into the collection
+//     const result = await Participant.insertMany(
+//       attendees.map(attendee => ({ name: attendee.profile.name, location: `${attendee.profile.addresses.home.city}, ${attendee.profile.addresses.home.region}`  }))
+//     );
+//     console.log("Eventbrite seed documents inserted into the collection");
 
-  } catch (err) {
-    console.error("No API Token");
-  } 
-}
+//   } catch (err) {
+//     console.error("No API Token");
+//   } 
+// }
 
-seed().catch(console.error);
+// seed().catch(console.error);
