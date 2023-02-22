80b055a0651886b4368c922e7071c577608a4c7c
Patrick Goral <patgoral@gmail.com>
2023-02-22 09:44:28 -0500

    server

--- seed.js
+++ seed.js
@@ -1,9 +1,9 @@
-const Participant = require('./models/participant')
+// const Participant = require('./models/participant')
 
-// Set up the Eventbrite API request parameters
-const token = process.env.EVENTBRITE_TOKEN
-const eventId = "430916953207";
-const url = `https://www.eventbriteapi.com/v3/events/${eventId}/attendees/`;
+// // Set up the Eventbrite API request parameters
+// const token = process.env.EVENTBRITE_TOKEN
+// const eventId = "430916953207";
+// const url = `https://www.eventbriteapi.com/v3/events/${eventId}/attendees/`;
 
 const headers = {
   "Authorization": `Bearer ${token}`
@@ -14,12 +14,22 @@ const headers = {
 // async function seed() {
 //         await Participant.deleteMany({})
 
+// const headers = {
+//   "Authorization": `Bearer ${token}`
+// };
 
-async function seed() {
-        await Participant.deleteMany({})
+// async function seed() {
+//         await Participant.deleteMany({})
 
   try {
 
+  
+//     // Fetch the attendee data from the Eventbrite API
+//     const response = await fetch(url, { headers });
+//     const data = await response.json();
+
+//     const attendees = data.attendees;
+//   try {
   
 //     // Fetch the attendee data from the Eventbrite API
 //     const response = await fetch(url, { headers });
